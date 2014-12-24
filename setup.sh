68980b27547514890ddd7f54336583235fb10f6a
Luan Nico <luanpotter28@hotmail.com>
2014-12-24 20:03:21 -0200

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,17 @@
-NAME='Luan'
-sed -i 's/{name}/$NAME/g' *.*
+replaceArg() {
+  echo "$1: "
+  read value
+  sed -i "s/{$1}/$value/g" bower.json package.json Gruntfile.js README.md
+}
+
+replaceArg 'name'
+replaceArg 'desc'
+replaceArg 'repo'
+replaceArg 'keywords'
+replaceArgs 'main-file'
+
+echo 'Done!'
+
+echo 'Deleting this script...'
+rm setup.sh
+
